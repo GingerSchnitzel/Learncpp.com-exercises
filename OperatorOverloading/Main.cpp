c958e33045b59eb50f051f6d52db7402cb79c6a4
GingerSchnitzel <141836349+GingerSchnitzel@users.noreply.github.com>
2024-06-22 16:42:13 +0300

    Overloaded operator!= for the Fraction class

--- OperatorOverloading/Main.cpp
+++ OperatorOverloading/Main.cpp
@@ -90,6 +90,12 @@ bool operator== (const Fraction& f1, const Fraction& f2)
 	return (f1.m_numerator == f2.m_numerator) && (f2.m_denominator == f2.m_denominator);
 }
 
+bool operator!= (const Fraction& f1, const Fraction& f2)
+{
+	return !(operator==(f1, f2));
+}
+
+
 
 int main()
 {
