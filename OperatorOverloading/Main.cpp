38a4e591bd1b66f6354de2a3d5f7d035d7f1c1fc
GingerSchnitzel <141836349+GingerSchnitzel@users.noreply.github.com>
2024-06-22 16:48:21 +0300

    Updated main function

--- OperatorOverloading/Main.cpp
+++ OperatorOverloading/Main.cpp
@@ -117,26 +117,15 @@ bool operator>= (const Fraction& f1, const Fraction& f2)
 
 int main()
 {
-	Fraction f1{ 2, 5 };
-	f1.print();
+	Fraction f1{ 3, 2 };
+	Fraction f2{ 5, 8 };
 
-	Fraction f2{ 3, 8 };
-	f2.print();
-
-	Fraction f3{ f1 * f2 };
-	f3.print();
-
-	Fraction f4{ f1 * 2 };
-	f4.print();
-
-	Fraction f5{ 2 * f2 };
-	f5.print();
-
-	Fraction f6{ Fraction{1, 2} *Fraction{2, 3} *Fraction{3, 4} };
-	f6.print();
-
-	Fraction f7{ 0, 6 };
-	f7.print();
+	std::cout << f1 << ((f1 == f2) ? " == " : " not == ") << f2 << '\n';
+	std::cout << f1 << ((f1 != f2) ? " != " : " not != ") << f2 << '\n';
+	std::cout << f1 << ((f1 < f2) ? " < " : " not < ") << f2 << '\n';
+	std::cout << f1 << ((f1 > f2) ? " > " : " not > ") << f2 << '\n';
+	std::cout << f1 << ((f1 <= f2) ? " <= " : " not <= ") << f2 << '\n';
+	std::cout << f1 << ((f1 >= f2) ? " >= " : " not >= ") << f2 << '\n';
 
 	return 0;
 }
