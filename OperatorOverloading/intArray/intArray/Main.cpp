5421c0afc08be7267c58789a7ffad930797b40ba
GingerSchnitzel <141836349+GingerSchnitzel@users.noreply.github.com>
2024-07-20 20:55:46 +0300

    Added tests

--- OperatorOverloading/intArray/intArray/Main.cpp
+++ OperatorOverloading/intArray/intArray/Main.cpp
@@ -33,6 +33,7 @@ public:
 	~IntArray()
 	{
 		delete[] m_array;
+		m_array = nullptr;
 	}
 
 	friend std::ostream& operator<< (std::ostream& out, const IntArray& array)
@@ -69,8 +70,34 @@ public:
 };
 
 
+IntArray fillArray()
+{
+	IntArray a(5);
+
+	a[0] = 5;
+	a[1] = 8;
+	a[2] = 2;
+	a[3] = 3;
+	a[4] = 6;
+
+	return a;
+}
+
 int main()
 {
+	IntArray a{ fillArray() };
+
+	std::cout << a << '\n';
+
+	auto ref{ a }; 
+	a = ref;
+
+	IntArray b(1);
+	b = a;
+
+	a[4] = 7;
+
+	std::cout << b << '\n';
 
 	return 0;
 }
